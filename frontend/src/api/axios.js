558ac6bf40d8ddf97eb153d5764c9ba2c13e81eb
imalescript <274859927+imalescript@users.noreply.github.com>
2026-04-17 16:44:34 -0400

    feat: inicializada la carpeta frontend

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept': 'application/json',
+    }
+});
+
+export default axiosInstance;
