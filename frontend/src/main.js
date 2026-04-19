11c27ae2ef18914774073ca946ca09a9bd7f5ef5
imalescript <274859927+imalescript@users.noreply.github.com>
2026-04-18 21:43:07 -0400

    feat: tabla de pacientes ahora tiene filtros y paginacion

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue';
 
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css'
 
 const app = createApp(App);
 const pinia = createPinia();
--- frontend/src/views/dashboard/DashboardView.vue
+++ frontend/src/views/dashboard/DashboardView.vue
@@ -1,28 +1,62 @@
 <script setup>
- import { usePatientStore } from '@/stores/patientStore';
- import { onMounted } from 'vue';
+import { usePatientStore } from '@/stores/patientStore';
+import { ref, onMounted } from 'vue';
+import { FilterMatchMode } from '@primevue/core/api';
 
 const patientStore = usePatientStore();
-
 onMounted(() => {
     patientStore.fetchPatients();
 });
+
+//FILTER
+const filters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+
 </script>
+
 <template>
     <DataTable
-        scrollable 
-        scrollHeight="flex" 
-        scrollDirection="both"
+        sortMode="multiple" removableSort
+        scrollable scrollHeight="flex" scrollDirection="both"
+        paginator :rows="10" :rowsPerPageOptions="[10, 20, 30, 40]"
         :value="patientStore.patients"
         :loading="patientStore.isLoading"
+        :filters="filters"
+        :globalFilterFields="['cedula', 'full_name', 'phone']"
     >
+
+    <template #header>
+      <div class="flex flex-wrap items-center justify-between gap-4">
+        
+        <Button 
+          icon="pi pi-plus" 
+          rounded
+          @click="" 
+        />
+
+        <IconField>
+          <InputIcon class="pi pi-search" />
+          <InputText
+            id="filter-input"
+            v-model="filters['global'].value" 
+            placeholder="Buscar..." 
+            class="w-full sm:w-64"
+          />
+        </IconField>
+
+      </div>
+    </template>
+
         <Column 
+            sortable
             field="cedula" 
             header="Cédula"
             headerClass="min-w-32"
         ></Column>
 
         <Column 
+            sortable
             field="full_name" 
             header="Nombre"
             headerClass="min-w-48"
@@ -30,44 +64,58 @@ onMounted(() => {
         ></Column>
 
         <Column 
+            sortable
             field="age" 
             header="Edad"
             headerClass="min-w-16"
         ></Column>
 
         <Column 
+            sortable
             field="gender" 
             header="Género"
             headerClass="min-w-32"
         ></Column>
 
         <Column 
+            sortable
             field="phone" 
             header="Teléfono"
             headerClass="min-w-48"
         ></Column>
 
         <Column 
+            sortable
             field="blood_type" 
             header="Sangre"
             headerClass="min-w-16"
-            ></Column>
+        ></Column>
+        
         <Column 
+            sortable
             field="" 
             header="Email"
             headerClass="min-w-48"
         ></Column>
 
         <Column 
+            sortable
             field="" 
             header="Dirección"
             headerClass="min-w-48"
         ></Column>
 
-        <Column 
-            field="" 
-            header="Condiciones"
-            headerClass="min-w-64"
-        ></Column>
+        <Column
+        header="Acción"        
+        header-class="min-w-48"
+        >
+            <template #body="slotProps">
+                <div class="flex gap-1.5">
+                    <Button icon="pi pi-eye" rounded severity="info"/>
+                    <Button icon="pi pi-pencil" rounded />
+                    <Button icon="pi pi-trash" rounded  severity="danger"/>
+                </div>
+            </template>
+        </Column>
     </DataTable>
 </template>
